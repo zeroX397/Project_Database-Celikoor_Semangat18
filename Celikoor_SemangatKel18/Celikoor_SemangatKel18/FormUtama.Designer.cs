43638a1629faedbc9eb62b4a26b605b2487f4865
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2023-11-14 17:46:54 +0700

    Input Folder Proyek DB
    Input folder proyek yg berisi SLN dan Windows Forms.

---
+++ Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormUtama.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Celikoor_SemangatKel18
+{
+    partial class FormUtama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kasirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.konsumenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(639, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminToolStripMenuItem,
+            this.kasirToolStripMenuItem,
+            this.operatorToolStripMenuItem,
+            this.konsumenToolStripMenuItem});
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.userToolStripMenuItem.Text = "Login";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // kasirToolStripMenuItem
+            // 
+            this.kasirToolStripMenuItem.Name = "kasirToolStripMenuItem";
+            this.kasirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kasirToolStripMenuItem.Text = "Kasir";
+            // 
+            // operatorToolStripMenuItem
+            // 
+            this.operatorToolStripMenuItem.Name = "operatorToolStripMenuItem";
+            this.operatorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.operatorToolStripMenuItem.Text = "Operator";
+            // 
+            // konsumenToolStripMenuItem
+            // 
+            this.konsumenToolStripMenuItem.Name = "konsumenToolStripMenuItem";
+            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.konsumenToolStripMenuItem.Text = "Konsumen";
+            // 
+            // FormUtama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 447);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormUtama";
+            this.Text = "Celikoor 21 CIneplex";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kasirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem konsumenToolStripMenuItem;
+    }
+}
+
