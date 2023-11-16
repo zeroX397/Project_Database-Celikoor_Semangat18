ee5ff51d5961c5bf6c88c9c77c757bf88a79ae88
RyanSubagia <s160422024@student.ubaya.ac.id>
2023-11-16 22:39:44 +0700

    inpo

--- Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormUtama.Designer.cs
+++ Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormUtama.Designer.cs
@@ -40,12 +40,14 @@ namespace Celikoor_SemangatKel18
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.userToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(639, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(719, 33);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -57,41 +59,42 @@ namespace Celikoor_SemangatKel18
             this.operatorToolStripMenuItem,
             this.konsumenToolStripMenuItem});
             this.userToolStripMenuItem.Name = "userToolStripMenuItem";
-            this.userToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(72, 29);
             this.userToolStripMenuItem.Text = "Login";
             // 
             // adminToolStripMenuItem
             // 
             this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.adminToolStripMenuItem.Text = "Admin";
             // 
             // kasirToolStripMenuItem
             // 
             this.kasirToolStripMenuItem.Name = "kasirToolStripMenuItem";
-            this.kasirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kasirToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.kasirToolStripMenuItem.Text = "Kasir";
             // 
             // operatorToolStripMenuItem
             // 
             this.operatorToolStripMenuItem.Name = "operatorToolStripMenuItem";
-            this.operatorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.operatorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.operatorToolStripMenuItem.Text = "Operator";
             // 
             // konsumenToolStripMenuItem
             // 
             this.konsumenToolStripMenuItem.Name = "konsumenToolStripMenuItem";
-            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
             this.konsumenToolStripMenuItem.Text = "Konsumen";
             // 
             // FormUtama
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 447);
+            this.ClientSize = new System.Drawing.Size(719, 559);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormUtama";
             this.Text = "Celikoor 21 CIneplex";
             this.menuStrip1.ResumeLayout(false);
