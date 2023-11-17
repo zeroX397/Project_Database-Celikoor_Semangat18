9b7b51945e3b540ecdbbeef22c7f19006b651d56
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2023-11-17 15:18:03 +0700

    Mengedit MDI Forms dan Menustrips

--- Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormMainMenu.Designer.cs
+++ Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormMainMenu.Designer.cs
@@ -29,9 +29,6 @@ namespace tes
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.masterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.registerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,51 +39,11 @@ namespace tes
             this.kasirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.operatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.RecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.buttonLogin = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.labelNam = new System.Windows.Forms.Label();
-            this.panel.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel
-            // 
-            this.panel.BackColor = System.Drawing.Color.DarkSlateBlue;
-            this.panel.Controls.Add(this.label2);
-            this.panel.Controls.Add(this.label1);
-            this.panel.Controls.Add(this.menuStrip1);
-            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel.Location = new System.Drawing.Point(0, 0);
-            this.panel.Margin = new System.Windows.Forms.Padding(2);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(573, 106);
-            this.panel.TabIndex = 14;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(73, 70);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 22);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Celikoor";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(9, 47);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 22);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Welcome to the ";
             // 
             // menuStrip1
             // 
@@ -120,7 +77,7 @@ namespace tes
             // konsumenToolStripMenuItem
             // 
             this.konsumenToolStripMenuItem.Name = "konsumenToolStripMenuItem";
-            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.konsumenToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
             this.konsumenToolStripMenuItem.Text = "Konsumen";
             this.konsumenToolStripMenuItem.Click += new System.EventHandler(this.konsumenRegister_ToolStripMenuItem_Click);
             // 
@@ -138,28 +95,28 @@ namespace tes
             // konsumenToolStripMenuItem1
             // 
             this.konsumenToolStripMenuItem1.Name = "konsumenToolStripMenuItem1";
-            this.konsumenToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.konsumenToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
             this.konsumenToolStripMenuItem1.Text = "Konsumen";
             this.konsumenToolStripMenuItem1.Click += new System.EventHandler(this.konsumenLogin_ToolStripMenuItem_Click);
             // 
             // adminToolStripMenuItem
             // 
             this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
             this.adminToolStripMenuItem.Text = "Admin";
             this.adminToolStripMenuItem.Click += new System.EventHandler(this.adminToolStripMenuItem_Click);
             // 
             // kasirToolStripMenuItem1
             // 
             this.kasirToolStripMenuItem1.Name = "kasirToolStripMenuItem1";
-            this.kasirToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.kasirToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
             this.kasirToolStripMenuItem1.Text = "Kasir";
             this.kasirToolStripMenuItem1.Click += new System.EventHandler(this.kasirToolStripMenuItem_Click);
             // 
             // operatorToolStripMenuItem
             // 
             this.operatorToolStripMenuItem.Name = "operatorToolStripMenuItem";
-            this.operatorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.operatorToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
             this.operatorToolStripMenuItem.Text = "Operator";
             this.operatorToolStripMenuItem.Click += new System.EventHandler(this.operatorToolStripMenuItem_Click);
             // 
@@ -169,96 +126,62 @@ namespace tes
             this.RecordToolStripMenuItem.Size = new System.Drawing.Size(56, 22);
             this.RecordToolStripMenuItem.Text = "Record";
             // 
-            // pictureBox2
+            // label1
             // 
-            this.pictureBox2.BackgroundImage = global::tes.Properties.Resources.CELIKOOR_removebg_preview;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.InitialImage = null;
-            this.pictureBox2.Location = new System.Drawing.Point(8, 110);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(181, 176);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(93, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to the Celikoor Cinema Apps";
             // 
-            // buttonLogin
+            // panel
             // 
-            this.buttonLogin.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLogin.Location = new System.Drawing.Point(457, 252);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(2);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(107, 34);
-            this.buttonLogin.TabIndex = 15;
-            this.buttonLogin.Text = "Login";
-            this.buttonLogin.UseVisualStyleBackColor = true;
-            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.label5.Location = new System.Drawing.Point(193, 110);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(165, 22);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Selamat datang ";
-            // 
-            // labelNam
-            // 
-            this.labelNam.AutoSize = true;
-            this.labelNam.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNam.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.labelNam.Location = new System.Drawing.Point(362, 112);
-            this.labelNam.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.labelNam.Name = "labelNam";
-            this.labelNam.Size = new System.Drawing.Size(57, 19);
-            this.labelNam.TabIndex = 16;
-            this.labelNam.Text = "nama";
+            this.panel.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.menuStrip1);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(573, 106);
+            this.panel.TabIndex = 14;
             // 
             // FormMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(573, 313);
-            this.Controls.Add(this.labelNam);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.panel);
-            this.Controls.Add(this.pictureBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormMainMenu";
             this.Text = "FormMainMenu";
             this.Load += new System.EventHandler(this.FormMainMenu_Load);
-            this.panel.ResumeLayout(false);
-            this.panel.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button buttonLogin;
-        private System.Windows.Forms.Label label5;
+
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem masterToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem registerToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem RecordToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
-        private System.Windows.Forms.Label labelNam;
         private System.Windows.Forms.ToolStripMenuItem konsumenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem konsumenToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem kasirToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem operatorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem RecordToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
     }
 }
