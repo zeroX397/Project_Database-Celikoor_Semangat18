b6cc5cc49d22ee866d151ec52a010b1d5f1f0afd
AcediaArmagedon <s160422055@student.ubaya.ac.id>
2023-12-08 17:56:04 +0700

    perbaikan
    perbaikan karena tabrakan

--- Celikoor_SemangatKel18/Celikoor_Library/Aktor.cs
+++ Celikoor_SemangatKel18/Celikoor_Library/Aktor.cs
@@ -10,45 +10,27 @@ namespace Celikoor_Library
     {
         #region DATAMEMBERS
         private string name;
-<<<<<<< Updated upstream
         private DateTime birthdate;
         private string nationality;
-        private string filmography;
-        #endregion
-
-        #region CONSTRUCTORS
-        public Aktor(string name, DateTime birthdate, string nationality, string filmography)
-        {
-            this.Name = name;
-            this.Birthdate = birthdate;
-            this.Nationality = nationality;
-            this.Filmography = filmography;
-=======
-        private DateTime birthDate;
-        private string nationality;
-        private string filmography; //histori aktor main di film apa
+        private string filmography;//histori aktor main di film apa
         #endregion
 
         #region CONSTRUCTOR
         public Aktor(string name, DateTime birthDate, string nationality, string filmography)
         {
             Name = name;
-            BirthDate = birthDate;
+            Birthdate = birthDate;
             Nationality = nationality;
             Filmography = filmography;
->>>>>>> Stashed changes
         }
         #endregion
 
         #region PROPERTIES
         public string Name { get => name; set => name = value; }
-<<<<<<< Updated upstream
-        public DateTime Birthdate { get => birthdate; set => birthdate = value; }
-=======
-        public DateTime BirthDate { get => birthDate; set => birthDate = value; }
->>>>>>> Stashed changes
+
         public string Nationality { get => nationality; set => nationality = value; }
         public string Filmography { get => filmography; set => filmography = value; }
+        public DateTime Birthdate { get => birthdate; set => birthdate = value; }
         #endregion
 
         #region METHODS
