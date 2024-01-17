cc3e3b96ec9900711a5b25105f76a9534f69a23c
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-01-17 14:34:42 +0700

    Commit Terakhir

--- Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormInvoice.Designer.cs
+++ Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormInvoice.Designer.cs
@@ -47,10 +47,10 @@ namespace Celikoor_Semangat18
             this.panel1.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.panel1.Controls.Add(this.buttonExit);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(5, 6);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(4, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(991, 70);
+            this.panel1.Size = new System.Drawing.Size(743, 57);
             this.panel1.TabIndex = 41;
             // 
             // buttonExit
@@ -60,10 +60,10 @@ namespace Celikoor_Semangat18
             this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonExit.Font = new System.Drawing.Font("Verdana", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonExit.ForeColor = System.Drawing.Color.White;
-            this.buttonExit.Location = new System.Drawing.Point(928, 18);
-            this.buttonExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonExit.Location = new System.Drawing.Point(696, 15);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(36, 32);
+            this.buttonExit.Size = new System.Drawing.Size(27, 26);
             this.buttonExit.TabIndex = 23;
             this.buttonExit.Text = "X";
             this.buttonExit.UseVisualStyleBackColor = false;
@@ -74,21 +74,22 @@ namespace Celikoor_Semangat18
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(20, 18);
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 34);
+            this.label1.Size = new System.Drawing.Size(168, 26);
             this.label1.TabIndex = 19;
             this.label1.Text = "Halaman Kasir";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(19, 160);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 130);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(956, 276);
+            this.dataGridView1.Size = new System.Drawing.Size(717, 224);
             this.dataGridView1.TabIndex = 48;
             // 
             // buttonCari
@@ -96,23 +97,24 @@ namespace Celikoor_Semangat18
             this.buttonCari.BackColor = System.Drawing.Color.Transparent;
             this.buttonCari.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCari.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.buttonCari.Location = new System.Drawing.Point(528, 96);
-            this.buttonCari.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonCari.Location = new System.Drawing.Point(396, 78);
+            this.buttonCari.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCari.Name = "buttonCari";
-            this.buttonCari.Size = new System.Drawing.Size(96, 49);
+            this.buttonCari.Size = new System.Drawing.Size(72, 40);
             this.buttonCari.TabIndex = 43;
             this.buttonCari.Text = "Cari";
             this.buttonCari.UseVisualStyleBackColor = false;
+            this.buttonCari.Click += new System.EventHandler(this.buttonCari_Click);
             // 
             // buttonUbah
             // 
             this.buttonUbah.BackColor = System.Drawing.Color.Transparent;
             this.buttonUbah.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonUbah.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.buttonUbah.Location = new System.Drawing.Point(31, 469);
-            this.buttonUbah.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonUbah.Location = new System.Drawing.Point(23, 381);
+            this.buttonUbah.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonUbah.Name = "buttonUbah";
-            this.buttonUbah.Size = new System.Drawing.Size(137, 49);
+            this.buttonUbah.Size = new System.Drawing.Size(103, 40);
             this.buttonUbah.TabIndex = 42;
             this.buttonUbah.Text = "Ubah";
             this.buttonUbah.UseVisualStyleBackColor = false;
@@ -121,10 +123,9 @@ namespace Celikoor_Semangat18
             // textBoxCari
             // 
             this.textBoxCari.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxCari.Location = new System.Drawing.Point(289, 105);
-            this.textBoxCari.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxCari.Location = new System.Drawing.Point(217, 85);
             this.textBoxCari.Name = "textBoxCari";
-            this.textBoxCari.Size = new System.Drawing.Size(209, 30);
+            this.textBoxCari.Size = new System.Drawing.Size(158, 26);
             this.textBoxCari.TabIndex = 51;
             // 
             // comboBoxCari
@@ -132,28 +133,26 @@ namespace Celikoor_Semangat18
             this.comboBoxCari.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxCari.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxCari.FormattingEnabled = true;
-            this.comboBoxCari.Location = new System.Drawing.Point(98, 105);
-            this.comboBoxCari.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxCari.Location = new System.Drawing.Point(74, 85);
             this.comboBoxCari.Name = "comboBoxCari";
-            this.comboBoxCari.Size = new System.Drawing.Size(160, 33);
+            this.comboBoxCari.Size = new System.Drawing.Size(121, 28);
             this.comboBoxCari.TabIndex = 50;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(26, 108);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(20, 88);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.Size = new System.Drawing.Size(49, 20);
             this.label2.TabIndex = 49;
             this.label2.Text = "Cari : ";
             // 
             // FormInvoice
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 547);
+            this.ClientSize = new System.Drawing.Size(764, 444);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonCari);
@@ -161,6 +160,7 @@ namespace Celikoor_Semangat18
             this.Controls.Add(this.textBoxCari);
             this.Controls.Add(this.comboBoxCari);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormInvoice";
             this.Text = "FormInvoice";
             this.Load += new System.EventHandler(this.FormInvoice_Load);
--- Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormLaporan.cs
+++ Celikoor_SemangatKel18/Celikoor_SemangatKel18/FormLaporan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 using Celikoor_Library;
 
 namespace Celikoor_Semangat18
@@ -37,7 +38,19 @@ namespace Celikoor_Semangat18
 
         private void buttonFilmTerlaris_Click(object sender, EventArgs e)
         {
+            /*dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            string sql = "SELECT films.judul, SUM(films.id) as total , monthname(invoices.tanggal) as month\r\n" +
+                "FROM invoices\r\nINNER JOIN tikets on tikets.invoices_id = invoices.id\r\nINNER JOIN sesi_films as sesifilms on sesifilms.films_id = tikets.films_id\r\nINNER JOIN films on films.id = sesifilms.films_id\r\nWHERE YEAR(invoices.tanggal) = '2023'\r\nGROUP BY films.judul, month\r\nORDER BY month desc, total desc;\r\n";
+            MySqlDataReader hasil = Koneksi.JalankanPerintahSelect(sql);
+            dataGridView1.Columns.Add("colJudul", "Judul");
+            dataGridView1.Columns.Add("colTotal", "Total");
+            dataGridView1.Columns.Add("colMonth", "Month");
 
+            while (hasil.Read())
+            {
+                dataGridView1.Rows.Add(hasil.GetValue(0).ToString(), hasil.GetValue(1).ToString(), hasil.GetValue(2).ToString());
+            }*/
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
